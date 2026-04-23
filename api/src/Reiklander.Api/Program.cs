ecc963d13d078e8fc8d7c62db98afa1b7fb5c149
Jens Henrik Vogelius <jens.vogelius@lego.com>
2026-04-23 21:19:55 +0200

    created api project, added scalar

---
+++ api/src/Reiklander.Api/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.OpenApi;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi("v1", options =>
+{
+    options.AddDocumentTransformer((doc, _, _) =>
+    {
+        doc.Info = new OpenApiInfo
+        {
+            Title = "Reiklander API",
+            Version = "1.0",
+            Description = "API for Reiklander, an digital character sheet for WFRP 4th Edition."
+        };
+        return Task.CompletedTask;
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi("/openapi/v1.json");
+    app.MapScalarApiReference();
+}
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
